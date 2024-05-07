15c5c983db1044726da58ac0ca843bd93890c681
siredwardspike <siredwardspike@gmail.com>
2024-05-08 01:14:43 +0300

    Signin and Signup frontend

--- app/account/signIn.js
+++ app/account/signIn.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Icon from 'react-native-elements/dist/icons/Icon';
 
 
-export default function signUp() {
+export default function signIn() {
     const { height, width, fontScale } = useWindowDimensions();
 
   return (
@@ -21,21 +21,14 @@ export default function signUp() {
               <Icon name='pin' type="material" color="#B3C8CF"/>
           </View>
 
-          <View style={{flexDirection:'row-reverse',justifyContent:'space-between',alignItems:'center',borderRadius: 50, backgroundColor: 'white', borderWidth: 2, width: width * 0.8, borderColor: '#B3C8CF',padding:5}}>
-            <TextInput placeholder='Enter your profile url' textAlign='center' 
-              style={{fontSize: height * 0.02 , maxWidth:width * 0.6 ,flex:1,textAlign:'left'}}></TextInput>
-              <Icon name='image' type="material" color="#B3C8CF"/>
-          </View>
-
-
         <View style={{alignSelf:'center', fontSize:height*0.02}}>
 
             <Pressable >
-                <Text style={{textAlign:'center',borderWidth:2, borderRadius:50, borderColor: '#B3C8CF', backgroundColor: 'white',padding:5, fontWeight:'400',color:"#82aab9"}}>Sign Up</Text>
+                <Text style={{textAlign:'center',borderWidth:2, borderRadius:50, borderColor: '#B3C8CF', backgroundColor: 'white',padding:5, fontWeight:'400',color:"#82aab9"}}>Sign in</Text>
             </Pressable>
 
             <Pressable>
-                <Text style={{color:"#4D869C", fontWeight:'200'}}>Already registerd?</Text>
+                <Text style={{color:"#4D869C", fontWeight:'200'}}>Forgot your password?</Text>
             </Pressable>
 
         </View>
