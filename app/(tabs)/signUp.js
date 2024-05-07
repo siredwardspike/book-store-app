42dd04dd89f0c566c0896137cdf03a98ba3c4bc5
siredwardspike <siredwardspike@gmail.com>
2024-05-08 01:05:41 +0300

    Sign in and sign up frontend

---
+++ app/(tabs)/signUp.js
@@ -0,0 +1,47 @@
+import { View, Text ,Image,useWindowDimensions, Pressable ,TextInput} from 'react-native'
+import React from 'react'
+import Icon from 'react-native-elements/dist/icons/Icon';
+
+
+export default function signUp() {
+    const { height, width, fontScale } = useWindowDimensions();
+
+  return (
+    <View style={{flex:1,justifyContent:'center',alignSelf:'center',gap:20}}>
+
+       <View style={{flexDirection:'row-reverse',justifyContent:'space-between',alignItems:'center',borderRadius: 50, backgroundColor: 'white', borderWidth: 2, width: width * 0.8, borderColor: '#B3C8CF',padding:5}}>
+            <TextInput placeholder='Enter your email' textAlign='center'
+              style={{fontSize: height * 0.02 , maxWidth:width * 0.6 ,flex:1,textAlign:'left'}}></TextInput>
+              <Icon name='mail' type="material" color="#B3C8CF"/>
+          </View>
+
+          <View style={{flexDirection:'row-reverse',justifyContent:'space-between',alignItems:'center',borderRadius: 50, backgroundColor: 'white', borderWidth: 2, width: width * 0.8, borderColor: '#B3C8CF',padding:5}}>
+            <TextInput placeholder='Enter your password' textAlign='center' secureTextEntry={true}
+              style={{fontSize: height * 0.02 , maxWidth:width * 0.6 ,flex:1,textAlign:'left'}}></TextInput>
+              <Icon name='pin' type="material" color="#B3C8CF"/>
+          </View>
+
+          <View style={{flexDirection:'row-reverse',justifyContent:'space-between',alignItems:'center',borderRadius: 50, backgroundColor: 'white', borderWidth: 2, width: width * 0.8, borderColor: '#B3C8CF',padding:5}}>
+            <TextInput placeholder='Enter your profile url' textAlign='center' 
+              style={{fontSize: height * 0.02 , maxWidth:width * 0.6 ,flex:1,textAlign:'left'}}></TextInput>
+              <Icon name='image' type="material" color="#B3C8CF"/>
+          </View>
+
+
+        <View style={{alignSelf:'center', fontSize:height*0.02}}>
+
+            <Pressable >
+                <Text style={{textAlign:'center',borderWidth:2, borderRadius:50, borderColor: '#B3C8CF', backgroundColor: 'white',padding:5, fontWeight:'500',color:"#4D869C"}}>Sign Up</Text>
+            </Pressable>
+
+            <Pressable>
+                <Text style={{color:"#4D869C", fontWeight:'200'}}>Already registerd?</Text>
+            </Pressable>
+
+        </View>
+        
+
+        
+    </View>
+  )
+}
