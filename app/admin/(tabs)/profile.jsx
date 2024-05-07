25ee7ee82fe740b616cbfdfe31f1c9ba8e042656
siredwardspike <siredwardspike@gmail.com>
2024-05-08 01:16:25 +0300

    profile frontend

--- app/admin/(tabs)/profile.jsx
+++ app/admin/(tabs)/profile.jsx
@@ -1,24 +1,68 @@
 import { View, Text ,Image,useWindowDimensions, Pressable} from 'react-native'
 import React from 'react'
+import Icon from 'react-native-elements/dist/icons/Icon';
+
 
 export default function profile() {
     const {height, width,fontScale} = useWindowDimensions();
 
   return (
-    <View style={{flex:1,backgroundColor:'#B3C8CF',justifyContent:'center'}}> 
-      <View style={{flexDirection:'column',alignSelf:'center'}}>
-        <Image source={{uri:'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRaT2WozXdM9ChvdlD38Wp0--s6sgOqG4lbgwvrO5Ou16gUzNsE'}} 
-        style={{height:height>1200?height*0.5:height*0.3,width:width*0.3, borderRadius:100, aspectRatio:1, borderWidth:fontScale*5, borderColor:'#BED7DC'}}></Image>
-    <View style={{alignItems:'center'}}>
-        <Text style={{fontSize:fontScale*20 , fontWeight:'bold' ,color:'black'}}>User Name: The Rock</Text>
+    <View style={{flex:1,justifyContent:'center',alignContent:'flex-start',backgroundColor:'#EEF7FF'}}> 
+          <View style={{flexDirection:'row',alignSelf:'center'}}>
+            <View style={{alignItems:'center',padding:30,gap:5}}>
+                
+                
+                <View style={{borderRadius:10,padding:10,gap:10,alignItems:'center', marginBottom:20}}>
 
-        <Pressable> 
-             <Text style={{fontSize:fontScale*20 , fontWeight:'100' ,color:'black'}}>Sign Out</Text>
-        </Pressable>
-    </View>
+                <Image source={{uri:'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRaT2WozXdM9ChvdlD38Wp0--s6sgOqG4lbgwvrO5Ou16gUzNsE'}} 
+                style={{height:height>1200?height*0.25:height*0.15,width:width*0.15, borderRadius:30, aspectRatio:1}}></Image>
+
+                      <View style={{flexDirection:'row',alignItems:'center',alignSelf:'flex-start'}}>
+                      <Icon name='person' type="material" color="#2C4E70" style={{ margin:3 }} />
+                        <Text style={{fontSize:fontScale*20 , fontWeight:'normal' ,color:'black'}}>The Rock</Text>
+                      </View>
+
+
+
+                      <View style={{flexDirection:'row',alignItems:'center',alignSelf:'flex-start'}}>
+                      <Icon name='mail' type="material" color="#2C4E70" style={{ margin:3 }} />
+                        <Text style={{fontSize:fontScale*20 , fontWeight:'normal' ,color:'black'}}>dwaynejohnson@rock.com</Text>
+                      </View>
+
+                </View>
+                
+                <View style={{flexDirection:'column',alignItems:'flex-start',alignSelf:'center'}}>
+
+
+                    <Pressable style={{flexDirection:'row-reverse',alignItems:'center'}}> 
+                          <Text style={{fontSize:height*0.02 , fontWeight:'600' ,color:'#4D869C'}}>Cart history</Text>
+                          <Icon name="shopping-cart" type="material" color="#4D869C" style={{ margin:3 }} />
+                      </Pressable>
+
+                      <Pressable style={{flexDirection:'row-reverse',alignItems:'center'}}> 
+                          <Text style={{fontSize:height*0.02 , fontWeight:'400' ,color:'#4D869C'}}>Whishlist</Text>
+                          <Icon name="list" type="material" color="#4D869C" style={{ margin:3 }} />
+                      </Pressable>
+
+                      <Pressable style={{flexDirection:'row-reverse',alignItems:'center'}}> 
+                          <Text style={{fontSize:height*0.02 , fontWeight:'400' ,color:'#4D869C'}}>Change password</Text>
+                          <Icon name="pin" type="material" color="#4D869C" style={{ margin:3 }} />
+                      </Pressable>
+
+                      <Pressable style={{flexDirection:'row-reverse',alignItems:'center'}}> 
+                          <Text style={{fontSize:height*0.02 , fontWeight:'400' ,color:'#4D869C'}}>Signout</Text>
+                          <Icon name="logout" type="material" color="#4D869C" style={{ margin:3 }} />
+                      </Pressable>
+                
+                
+                </View>                
+                
+
+
+            </View>
    
      
-    </View>
+          </View>
 
     </View>
   
