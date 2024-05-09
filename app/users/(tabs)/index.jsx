41f6f9cefd7ddf454b2b77f2661869732b55b0b4
omar khaled <123329764+Ozy20@users.noreply.github.com>
2024-05-09 16:44:55 +0300

    "bugs fixed"

--- app/users/(tabs)/index.jsx
+++ app/users/(tabs)/index.jsx
@@ -4,7 +4,7 @@ import Item from '../../../components/userBookItem';
 import Icon from 'react-native-elements/dist/icons/Icon';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { router,Link } from 'expo-router';
-
+import AppHeader from '../../../components/AppHeader';
 let color = "#ccc";
 
 let books = [{ id: 0, name: "book1", author: "Segara", category: "science",price:120,favorite:false }
