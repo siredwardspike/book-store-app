7db3f7c6b6e2e565b6896c9221ede4104b83f460
siredwardspike <siredwardspike@gmail.com>
2024-05-10 23:25:52 +0300

    Removed redundant code

--- app/index.js
+++ app/index.js
@@ -26,24 +26,4 @@ export default function Page() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+
