c2b88f165afb924fb1e6aed06a0aec673990e79c
omar khaled <123329764+Ozy20@users.noreply.github.com>
2024-05-12 12:23:41 +0300

    .

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -19,21 +19,6 @@ export default function AppHeader() {
   return (
     <View style={{ padding: 5, backgroundColor: 'white', gap: 5, flexDirection: 'row', justifyContent: 'space-between' }}>
 
-      <Text style={{ fontSize: imageWidth * imageHeight * 0.002, color: '#2C4E70', fontWeight: 'bold' }}>Book Store</Text>
-
-
-      <View style={{ flexDirection: "row" }}>
-        <Pressable onPress={() => { router.navigate(`/users/profile`) }} style={{ borderWidth: 2, padding: 5, borderRadius: 10, backgroundColor: 'white', borderColor: '#B3C8CF', width: 38, height: 38 }}>
-          <Icon name='account-circle' type="material" color="#2C4E70" />
-        </Pressable>
-        <Pressable onPress={() => { router.navigate(`/users/cart`) }} style={{ marginHorizontal: 5, borderWidth: 2, padding: 5, borderRadius: 10, backgroundColor: 'white', borderColor: '#B3C8CF', width: 38, height: 38 }}>
-          <Icon name='shopping-cart' type="material" color="#2C4E70" />
-        </Pressable>
-        <Pressable onPress={() => { router.navigate(`/users/results`) }} style={{ borderWidth: 2, padding: 5, borderRadius: 10, backgroundColor: 'white', borderColor: '#B3C8CF', width: 38, height: 38 }}>
-          <Icon name='search' type="material" color="#2C4E70" />
-        </Pressable>
-
-      </View>
 
 
 
