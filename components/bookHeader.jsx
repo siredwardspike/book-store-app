ee11b4345c3a05f1fa4176bfa247b1338becb108
omar khaled <123329764+Ozy20@users.noreply.github.com>
2024-05-12 12:24:47 +0300

    ""

--- components/bookHeader.jsx
+++ components/bookHeader.jsx
@@ -13,19 +13,6 @@ export default function BookHeader(link) {
   return (
     <View style={{ padding: 5, backgroundColor: 'white', gap: 5, flexDirection: 'row', justifyContent: 'space-between' }}>
 
-    <Text style={{ fontSize: imageWidth*imageHeight*0.002, color: '#2C4E70', fontWeight: 'bold' }}>Book Store</Text>
-    <View style={{flexDirection:'row'}}>
-        <Link href='/' asChild>
-            <Pressable style={{ borderWidth: 2, marginHorizontal: 5, borderRadius: 10, backgroundColor: 'white', borderColor: '#B3C8CF', width: 38, height: 38, justifyContent: "center", alignContent: "center" }} >
-                <Icon name='arrow-back' type="material" color="#2C4E70" />
-            </Pressable>
-        </Link>
-        <Link href='/users/results' asChild>
-            <Pressable style={{ borderWidth: 2, padding: 5, borderRadius: 10, backgroundColor: 'white', borderColor: '#B3C8CF',width: 38, height: 38 }}>
-                <Icon name='search' type="material" color="#2C4E70" />
-            </Pressable>
-        </Link >
-    </View>
 
 
 
