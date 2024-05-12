52c2165fbd48d0f5916ad68ff477cd138b04f53d
omar khaled <123329764+Ozy20@users.noreply.github.com>
2024-05-12 12:25:33 +0300

    "book header component"

--- components/bookHeader.jsx
+++ components/bookHeader.jsx
@@ -2,20 +2,33 @@ import { View, Text, useWindowDimensions, Pressable, TextInput } from 'react-nat
 import React from 'react'
 import { Image } from 'react-native';
 import Icon from 'react-native-elements/dist/icons/Icon';
-import { router,Link } from 'expo-router';
+import { router, Link } from 'expo-router';
 
 export default function BookHeader(link) {
-  const { height, width, fontScale } = useWindowDimensions();
+    const { height, width, fontScale } = useWindowDimensions();
 
-  let imageWidth = width > 1200 ? width * 0.1 : width * 0.28;
-  let imageHeight = height > 900 ? height * 0.08 : height * 0.2;
+    let imageWidth = width > 1200 ? width * 0.1 : width * 0.28;
+    let imageHeight = height > 900 ? height * 0.08 : height * 0.2;
 
-  return (
-    <View style={{ padding: 5, backgroundColor: 'white', gap: 5, flexDirection: 'row', justifyContent: 'space-between' }}>
-
-
+    return (
+        <View style={{ padding: 5, backgroundColor: 'white', gap: 5, flexDirection: 'row', justifyContent: 'space-between' }}>
 
 
-</View>
-  )
+            <Text style={{ fontSize: imageWidth * imageHeight * 0.002, color: '#2C4E70', fontWeight: 'bold' }}>Book Store</Text>
+            <View style={{ flexDirection: 'row' }}>
+                <Link href='/' asChild>
+                    <Pressable style={{ borderWidth: 2, marginHorizontal: 5, borderRadius: 10, backgroundColor: 'white', borderColor: '#B3C8CF', width: 38, height: 38, justifyContent: "center", alignContent: "center" }} >
+                        <Icon name='arrow-back' type="material" color="#2C4E70" />
+                    </Pressable>
+                </Link>
+                <Link href='/users/results' asChild>
+                    <Pressable style={{ borderWidth: 2, padding: 5, borderRadius: 10, backgroundColor: 'white', borderColor: '#B3C8CF', width: 38, height: 38 }}>
+                        <Icon name='search' type="material" color="#2C4E70" />
+                    </Pressable>
+                </Link >
+            </View>
+
+
+        </View>
+    )
 }
